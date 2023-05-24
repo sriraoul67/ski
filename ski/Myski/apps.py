6f8ea3e2fb0bc7799bca1e12e10e64f7602cb8c0
Antoine Reiminger <antoine.reiminger1@uha.fr>
2023-05-24 14:37:23 +0200

    Initial commit

---
+++ ski/Myski/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class MyskiConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'Myski'
---
+++ ski/ski/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for ski project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ski.settings')
+
+application = get_asgi_application()
